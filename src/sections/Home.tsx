02ed091a187b7aac96c789a19717270f7ede0c9e
unknown <raphael.salayog@philcentro.ph>
2025-08-28 16:20:01 +0800

    feat: change resume link

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -36,7 +36,7 @@ const Home = () => {
                         <Button
                             onClick={() =>
                                 window.open(
-                                    "https://drive.google.com/file/d/1ntCtkwHnQxh5D6TN1fx7NhikcXL4nuSc/view?usp=sharing",
+                                    "https://drive.google.com/file/d/1p-EGgKvtNZqTxMAZf2jKW04h6ZDiMuXC/view?usp=sharing",
                                     "_blank",
                                     "noopener,noreferrer"
                                 )
